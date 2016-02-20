856eff61ed2488326685ff116ea5fa64c3b757d7
Stephen Prill <stephen.prill@gmail.com>
2016-02-20 12:34:06 -0700

    created user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220181656) do
+ActiveRecord::Schema.define(version: 20160220193203) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
