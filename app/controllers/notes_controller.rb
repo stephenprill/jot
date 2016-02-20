df1323fbde3f4339dc43acf467a6b4d912769b5d
Stephen Prill <stephen.prill@gmail.com>
2016-02-20 12:06:16 -0700

    update for notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -26,6 +26,11 @@ class NotesController < ApplicationController
   end
 
   def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
   end
 
   def destroy
