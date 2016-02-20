07dabd8e9d620e25dd1e86169f3ebf9cb99ed10d
Stephen Prill <stephen.prill@gmail.com>
2016-02-20 12:50:51 -0700

    updated routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
   get 'users/new'
-
   get 'welcome/index'
-
   root 'welcome#index'
-
   resources :notes
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  resources :sessions
 end
