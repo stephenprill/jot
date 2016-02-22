a677369c5e6c46b1c970b2fde97f65a8ad7652d0
Stephen Prill <stephen.prill@gmail.com>
2016-02-21 21:10:00 -0700

    updated notes index and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, :notice => "Signed up"
+      redirect_to log_in_path, :notice => "You've successfully signed up, please login"
     else
       render "new"
     end
